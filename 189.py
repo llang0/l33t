ccbd298e53ec36a47f029119925ee40d4478a237
llang0 <leslielang@alum.calarts.edu>
2024-05-10 11:13:16 -0700

    added another solution to 189

--- 189.py
+++ 189.py
@@ -17,6 +17,45 @@ def rotate(nums, k):
     for i in range(len(nums)):
         nums[i] = newArray[i]
     
+def rotate2(nums, k):
+    pass
+
+    # step 1: reverse array
+    l = 0
+    r = len(nums)-1
+    while r > l:
+        temp = nums[l]
+        nums[l] = nums[r]
+        nums[r] = temp 
+        l += 1
+        r -= 1
+
+    # step 2, reverse the first k items
+    l = 0
+    r = k-1
+    while r > l:
+        temp = nums[l]
+        nums[l] = nums[r]
+        nums[r] = temp
+        l += 1
+        r -= 1
+    
+    # step 3, reverse the last len-k items
+    l = k
+    r = len(nums)-1
+    while r > l:
+        temp = nums[l]
+        nums[l] = nums[r]
+        nums[r] = temp
+        l += 1
+        r -= 1
+
+
 nums = [1,2,3,4,5,6,7]
 rotate(nums, 3)
 print(nums)
+
+nums = [1,2,3,4,5,6,7]
+rotate2(nums, 3)
+print(nums)
+
